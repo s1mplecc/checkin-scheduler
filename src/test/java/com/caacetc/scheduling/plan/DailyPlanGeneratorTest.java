701c2609f2c0b60d765e7c31b3eb032ac9faae5a
s1mple <798342418@qq.com>
2019-03-17 12:36:59 +0800

    init project

--- src/test/java/com/caacetc/scheduling/plan/DailyPlanGeneratorTest.java
+++ src/test/java/com/caacetc/scheduling/plan/DailyPlanGeneratorTest.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 public class DailyPlanGeneratorTest {
     @Test
-    public void should_() {
+    public void should_schedule_staff_correctly() {
         DailyPlansGenerator generator = new DailyPlansGenerator(2, 10);
         List<DailyPlan> dailyPlans = generator.generate(31);
-        Scheduling scheduling = new Scheduling(dailyPlans);
         System.out.println(generator);
 
+        Scheduling scheduling = new Scheduling(dailyPlans);
         scheduling.schedule();
         for (Staff staff : scheduling.staffs()) {
             System.out.println(staff);
