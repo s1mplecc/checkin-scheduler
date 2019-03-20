1a766d3d374e68f6c4291006d6fa6886813fb7e0
s1mple <798342418@qq.com>
2019-03-20 15:23:20 +0800

    parse departure time

--- src/main/java/com/caacetc/scheduling/plan/checkin/Flight.java
+++ src/main/java/com/caacetc/scheduling/plan/checkin/Flight.java
@@ -2,28 +2,35 @@ package com.caacetc.scheduling.plan.checkin;
 
 import lombok.ToString;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @ToString
 public class Flight {
     private final String date;
-    private final String departTime;
     private final String passengerNum;
     private final String region;
     private final String destination;
+    private Date departTime;
 
     private Integer economyCabinNum;
     private Integer premiumCabinNum;
 
     public Flight(String date, String departTime, String passengerNum, String region, String destination) {
         this.date = date;
-        this.departTime = departTime;
         this.passengerNum = passengerNum;
         this.region = region;
         this.destination = destination;
         this.economyCabinNum = 0;
         this.premiumCabinNum = 0;
+        try {
+            this.departTime = new SimpleDateFormat("yyyy/mm/dd hh:mm:ss").parse(departTime);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         computeCabinNumsBy(passengerNum);
     }
