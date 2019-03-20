3398a0c30901b994bc7967a86a1aa56f713c2cb3
s1mple <798342418@qq.com>
2019-03-20 11:35:12 +0800

    regex pattern match

--- src/main/java/com/caacetc/scheduling/plan/checkin/Flight.java
+++ src/main/java/com/caacetc/scheduling/plan/checkin/Flight.java
@@ -2,6 +2,9 @@ package com.caacetc.scheduling.plan.checkin;
 
 import lombok.ToString;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @ToString
 public class Flight {
     private String date;
@@ -9,10 +12,26 @@ public class Flight {
     private String passengerNum;
     private String region;
 
+    private Integer economyClassNum;
+    private Integer premiumNum;
+
     public Flight(String date, String departTime, String passengerNum, String region) {
         this.date = date;
         this.departTime = departTime;
         this.passengerNum = passengerNum;
         this.region = region;
+
+        parse(passengerNum);
+    }
+
+    public void parse(String passengerNum) {
+        Pattern compile = Pattern.compile(".*\\[Y:(\\d+),(\\d+),(\\d+)\\].*");
+        Matcher matcher = compile.matcher(passengerNum);
+        if (matcher.find()) {
+            System.out.println(matcher.group(0));
+            System.out.println(matcher.group(1));
+            System.out.println(matcher.group(2));
+            System.out.println(matcher.group(3));
+        }
     }
 }
---
+++ src/test/java/com/caacetc/scheduling/plan/checkin/FlightTest.java
@@ -0,0 +1,13 @@
+package com.caacetc.scheduling.plan.checkin;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class FlightTest {
+    @Test
+    public void should_() {
+        List<Flight> flights = new FlightMapper().flights();
+        System.out.println(flights.get(0));
+    }
+}
